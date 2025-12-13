3012144ab17ab16988a3214f87164d2f87cadc0e
Michal Herda <michalherda83@gmail.com>
2025-12-13 22:04:25 +0100

    add frontend scaffold

---
+++ front/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img
+          src="/toDoListLogo.png"
+          alt="Logo aplikacji"
+          className="app-logo"
+        />
+      </header>
+
+      <main className="app-content">
+        {/* tutaj później: login / inne strony */}
+      </main>
+    </div>
+  )
+}
+
+export default App
