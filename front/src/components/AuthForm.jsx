d993068404b35b10d2e6eb2eed832132dccc7640
Michal Herda <michalherda83@gmail.com>
2025-12-14 17:12:32 +0100

    start authentication errors implementation

--- front/src/components/AuthForm.jsx
+++ front/src/components/AuthForm.jsx
@@ -7,10 +7,32 @@ function AuthForm({ onAuthSuccess }) {
   const [showPassword, setShowPassword] = useState(false)
   const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [error, setError] = useState({})
+
   const isLogin = mode === 'login'
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    
+    setError({})
+
+    if (!password) {
+      setError({ password: 'Password is required' })
+      return
+    }
+
+    if (password.length < 5) {
+      setError({ password: 'Password must be at least 5 characters long' })
+      return
+    }
+
+    if (/*!isLogin && */password !== confirmPassword) {
+      setError({ confirmPassword: 'Passwords do not match' })
+      return
+    }
 
     // 🔧 tu później backend
     if (stayLoggedIn) {
@@ -24,12 +46,19 @@ function AuthForm({ onAuthSuccess }) {
     <form className="auth-form" onSubmit={handleSubmit}>
       <h2>{isLogin ? 'Login' : 'Sign up'}</h2>
 
-      <input type="text" placeholder="Enter your name" />
+      <input 
+        type="text" 
+        placeholder="Enter your name" 
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
 
       <div className="password-field">
         <input
             type={showPassword ? 'text' : 'password'}
             placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
         />
         <button
             type="button"
@@ -39,12 +68,21 @@ function AuthForm({ onAuthSuccess }) {
         >
             {showPassword ? '🙈' : '👁️'}
         </button>
+
+        {error.password && (
+          <div className="auth-error">
+            {error.password}
+          </div>
+        )}
       </div>
 
       <div className="password-field">
         <input
             type={showConfirmPassword ? 'text' : 'password'}
             placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className={error.password ? 'input-error' : ''}             // <--- do ogarnięcia
         />
         <button
             type="button"
@@ -54,6 +92,12 @@ function AuthForm({ onAuthSuccess }) {
         >
             {showConfirmPassword ? '🙈' : '👁️'}
         </button>
+
+        {error.confirmPassword && (
+        <div className="auth-error">
+          {error.confirmPassword}
+        </div>
+  )}
       </div>
 
 
